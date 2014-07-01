85607a14c9c2c8550e12ffdb27fd5fd25c0550ed
Parker Kemp <parker.kemp@hitcents.com>
2014-07-01 15:51:32 -0500

    Add insert/update/delete methods for Orders and Stores in Database.cs

--- Order.cs
+++ Order.cs
@@ -1,3 +1,6 @@
+using Mono.Data.Sqlite;
+using System;
+using System.Text.RegularExpressions;
 namespace SQLiteApp
 {
 	public class Order
@@ -19,6 +22,45 @@ namespace SQLiteApp
 			TotalCost = totalCost;
 			ContactName = contactName;
 			RushOrder = rushOrder;
+		}
+
+		public static string RandomID(Database database)
+		{
+			string id;
+			Random random = new Random();
+			do
+			{
+				id = "";
+				for (int i = 0; i < 32; i++)
+					id += random.Next(0, 15).ToString("X");
+			} while (!database.CheckUniqueOrderID(id));
+
+			return id;
+		}
+
+		public static bool ValidCost(string input)
+		{
+			return Regex.IsMatch(input, @"^[0-9]+(\.[0-9]{1,2})?$");
+		}
+
+		public static bool ValidTotalItems(string input)
+		{
+			return Regex.IsMatch(input, @"^[0-9]+$");
+		}
+
+		public static bool ValidRushOrder(string input)
+		{
+			return Regex.IsMatch(input, @"^[YNyn]$");
+		}
+
+		public static bool ValidDate(string input)
+		{
+			return Regex.IsMatch(input, @"^.+$");
+		}
+
+		public static bool ValidContactName(string input)
+		{
+			return Regex.IsMatch(input, @"^.+$");
 		}
 	}
 }
--- Store.cs
+++ Store.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System.Text.RegularExpressions;
 namespace SQLiteApp
 {
 	public class Store
@@ -53,6 +54,66 @@ namespace SQLiteApp
 			State = state;
 			Zip = zip;
 			TerritoryNum = territoryNum;
+		}
+
+		public static bool ValidStoreID(string input)
+		{
+			return Regex.IsMatch(input, "^[a-zA-Z0-9]{1,10}$");
+		}
+
+		public static bool ValidStoreName(string input)
+		{
+			return Regex.IsMatch(input, "^.{1,50}$");
+		}
+
+		public static bool ValidTerritoryNum(string input)
+		{
+			return Regex.IsMatch(input, "^[0-9]{1,5}$");
+		}
+
+		public static bool ValidSequenceNum(string input)
+		{
+			return Regex.IsMatch(input, "^[0-9]{0,4}$");
+		}
+
+		public static bool ValidStoreNum(string input)
+		{
+			return Regex.IsMatch(input, "^.{0,5}$");
+		}
+
+		public static bool ValidAddress1(string input)
+		{
+			return Regex.IsMatch(input, "^.{1,40}$");
+		}
+
+		public static bool ValidAddress2(string input)
+		{
+			return Regex.IsMatch(input, "^.{0,40}$");
+		}
+
+		public static bool ValidCity(string input)
+		{
+			return Regex.IsMatch(input, "^.{1,20}$");
+		}
+
+		public static bool ValidState(string input)
+		{
+			return Regex.IsMatch(input, "^[A-Za-z]{2,2}$");
+		}
+
+		public static bool ValidZip(string input)
+		{
+			return Regex.IsMatch(input, "^[0-9]{5,9}$");
+		}
+
+		public static bool ValidManagerName(string input)
+		{
+			return Regex.IsMatch(input, "^.{1,50}$");
+		}
+
+		public static bool ValidPhoneNum(string input)
+		{
+			return Regex.IsMatch(input, "^[0-9 -]{10,14}$");
 		}
 	}
 }
